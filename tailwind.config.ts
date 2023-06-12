afe336f0312712367b21b536ff31ef0a1c8744c3
James Prickitt <james@precision-websites.com>
2023-06-12 16:27:35 -0400

    feat: Added Tailwind forms, typography, daisyui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,5 +5,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography"),require('@tailwindcss/forms'), require('daisyui')],
 } satisfies Config;
