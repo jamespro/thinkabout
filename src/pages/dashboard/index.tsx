22a12bcb74b5b58c65d758144b751e6fe2283111
James Prickitt <james@precision-websites.com>
2023-06-14 22:45:17 -0400

    feat: Dashboard created; moved note management there and a sub page

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { type NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useSession } from "next-auth/react";
+
+import { DashboardHeader } from "~/components/DashboardHeader";
+
+const Profile: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Dashboard</title>
+        <meta
+          name="description"
+          content="Dashboard: Create, Read, Update, Delete"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <DashboardHeader headerText={"Dashboard"} />
+      <main>
+        <div className="m-6">
+          <ul>
+            <li>
+              <Link href="dashboard/manage" className="underline">
+                Manage notes
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default Profile;
